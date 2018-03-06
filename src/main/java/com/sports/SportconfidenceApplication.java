300cb9da1250be877bd6b6bdb4da720bbe3fe0e2
sahinakyol <akyol991@hotmail.com>
2018-03-07 00:04:57 +0300

    Created empty Project

---
+++ src/main/java/com/sports/SportconfidenceApplication.java
@@ -0,0 +1,12 @@
+package com.sports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportconfidenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportconfidenceApplication.class, args);
+	}
+}
