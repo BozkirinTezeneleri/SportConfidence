602fa066463c423e4d0a19261ad0efac14bc5a98
sahinakyol <akyol991@hotmail.com>
2018-04-23 14:11:52 +0300

    fixed some security problems about auth

--- src/main/java/com/sports/Config/SpringSecurityConfig.java
+++ src/main/java/com/sports/Config/SpringSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -39,7 +40,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-              //.antMatchers(HttpMethod.POST, "/users").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.POST, "/users").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/users/**").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/users/**").hasAuthority("ADMIN")
                 .anyRequest().authenticated()
                 .and().httpBasic()
                 .authenticationEntryPoint(authEntryPoint);
