b9b1c25fd0351160e076bbe4afbf2905a25c3c4e
sahinakyol <akyol991@hotmail.com>
2018-06-03 17:24:05 +0300

    migrated to postgres

--- src/main/java/com/sports/User/Model/User.java
+++ src/main/java/com/sports/User/Model/User.java
@@ -23,6 +23,7 @@ import java.util.Set;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "userPersonal")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
